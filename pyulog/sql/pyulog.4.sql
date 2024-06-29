18e0e9fd5e1643e87f2a9d1abd733cde7a655163
Herman Øie Kolden <herman@samfundet.no>
2024-06-29 08:58:50 +0200

    fix: dont drop all ulog data

--- pyulog/sql/pyulog.4.sql
+++ pyulog/sql/pyulog.4.sql
@@ -1,67 +1,37 @@
 BEGIN;
 PRAGMA foreign_keys=off;
 
--- Change REAL timestamps to INT. SQLITE only supports INT64, but ULog
+-- Change REAL timestamps to INT. SQLITE only supports INT64, but ULog -- changed from REAL
 -- timestamps are UINT64. We accept losing 1 bit at the top end, since 2^63
 -- microseconds = 400,000 years. which should be enough.
 
-CREATE TABLE IF NOT EXISTS ULog_tmp (
-        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-        SHA256Sum TEXT UNIQUE,
-        FileVersion INT,
-        StartTimestamp INT, -- changed from REAL
-        LastTimestamp INT, -- changed from REAL
-        CompatFlags TEXT,
-        IncompatFlags TEXT,
-        SyncCount INT,
-        HasSync BOOLEAN
-);
-INSERT OR IGNORE INTO ULog_tmp (Id, SHA256Sum, FileVersion, StartTimestamp, LastTimestamp, CompatFlags, IncompatFlags, SyncCount, HasSync) SELECT Id, SHA256Sum, FileVersion, StartTimestamp, LastTimestamp, CompatFlags, IncompatFlags, SyncCount, HasSync FROM ULog;
+ALTER TABLE ULog RENAME COLUMN StartTimestamp TO StartTimestamp_old;
+ALTER TABLE ULog ADD COLUMN StartTimestamp INT;
+UPDATE ULog SET StartTimestamp = CAST(StartTimestamp_old AS INT);
 
-CREATE TABLE IF NOT EXISTS ULogMessageDropout_tmp (
-        Timestamp INT, -- changed from REAL
-        Duration INT, -- changed from FLOAT
-        ULogId INT REFERENCES ULog (Id) ON DELETE CASCADE
-);
-INSERT OR IGNORE INTO ULogMessageDropout_tmp SELECT * FROM ULogMessageDropout;
+ALTER TABLE ULog RENAME COLUMN LastTimestamp TO LastTimestamp_old;
+ALTER TABLE ULog ADD COLUMN LastTimestamp INT;
+UPDATE ULog SET LastTimestamp = CAST(LastTimestamp_old AS INT);
 
-CREATE TABLE IF NOT EXISTS ULogMessageLogging_tmp (
-        LogLevel INT,
-        Timestamp INT,
-        Message TEXT,
-        ULogId INT REFERENCES ULog (Id) ON DELETE CASCADE
-);
-INSERT OR IGNORE INTO ULogMessageLogging_tmp SELECT * FROM ULogMessageLogging;
+ALTER TABLE ULogMessageDropout RENAME COLUMN Timestamp TO Timestamp_old;
+ALTER TABLE ULogMessageDropout ADD COLUMN Timestamp INT;
+UPDATE ULogMessageDropout SET Timestamp = CAST(Timestamp_old AS INT);
 
-CREATE TABLE IF NOT EXISTS ULogMessageLoggingTagged_tmp (
-        LogLevel INT,
-        Timestamp INT, -- changed from REAL
-        Tag INT,
-        Message TEXT,
-        ULogId INT REFERENCES ULog (Id) ON DELETE CASCADE
-);
-INSERT OR IGNORE INTO ULogMessageLoggingTagged_tmp SELECT * FROM ULogMessageLoggingTagged;
+ALTER TABLE ULogMessageDropout RENAME COLUMN Duration TO Duration_old;
+ALTER TABLE ULogMessageDropout ADD COLUMN Duration INT;
+UPDATE ULogMessageDropout SET Duration = CAST(Duration_old AS INT);
 
-CREATE TABLE IF NOT EXISTS ULogChangedParameter_tmp (
-        Timestamp INT, -- changed from REAL
-        Key TEXT,
-        Value BLOB,
-        ULogId INT REFERENCES ULog (Id) ON DELETE CASCADE
-);
-INSERT OR IGNORE INTO ULogChangedParameter_tmp SELECT * FROM ULogChangedParameter;
+ALTER TABLE ULogMessageLogging RENAME COLUMN Timestamp TO Timestamp_old;
+ALTER TABLE ULogMessageLogging ADD COLUMN Timestamp INT;
+UPDATE ULogMessageLogging SET Timestamp = CAST(Timestamp_old AS INT);
 
+ALTER TABLE ULogMessageLoggingTagged RENAME COLUMN Timestamp TO Timestamp_old;
+ALTER TABLE ULogMessageLoggingTagged ADD COLUMN Timestamp INT;
+UPDATE ULogMessageLoggingTagged SET Timestamp = CAST(Timestamp_old AS INT);
 
-DROP TABLE ULog;
-DROP TABLE ULogMessageDropout;
-DROP TABLE ULogMessageLogging;
-DROP TABLE ULogMessageLoggingTagged;
-DROP TABLE ULogChangedParameter;
-
-ALTER TABLE ULog_tmp RENAME TO ULog;
-ALTER TABLE ULogMessageDropout_tmp RENAME TO ULogMessageDropout;
-ALTER TABLE ULogMessageLogging_tmp RENAME TO ULogMessageLogging;
-ALTER TABLE ULogMessageLoggingTagged_tmp RENAME TO ULogMessageLoggingTagged;
-ALTER TABLE ULogChangedParameter_tmp RENAME TO ULogChangedParameter;
+ALTER TABLE ULogChangedParameter RENAME COLUMN Timestamp TO Timestamp_old;
+ALTER TABLE ULogChangedParameter ADD COLUMN Timestamp INT;
+UPDATE ULogChangedParameter SET Timestamp = CAST(Timestamp_old AS INT);
 
 PRAGMA foreign_keys=on;
 COMMIT;
